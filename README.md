e9fb6be5401e2dfca2318f6c198cd752dd4d2e48
Jeff Huang <82061589+solderq35@users.noreply.github.com>
2024-04-22 04:16:25 -0700

    Error checking (#3)
    * error checking
    
    * remove extra logs
    
    * formatting
    
    * png pls

--- README.md
+++ README.md
@@ -7,26 +7,42 @@ I'm hosting this via Node.JS on [Railway.app](https://railway.app/). For a simil
 For a full deep dive into the math behind this, please check [Time Calc Website README](https://github.com/solderq35/time-calc-under-5/blob/main/README.md#mathematics-code-explanation). This bot mostly works the same, except that this bot can return up to 4 possible values, in return for the user typing in less inputs than the Time Calc Website. I kept it that way as the original Timmy Bot did it the same, and also because the speedrunner users are generally ok with sifting through a few possible answers if they can enter less inputs.
 
 ## How to Run Your Own Instance
+
 - Before you start, create a `.env` file and include the line `DISCORD_TOKEN=DISCORD_TOKEN_VALUE`, where `DISCORD_TOKEN_VALUE` can be obtained as [explained here](https://www.online-tech-tips.com/computer-tips/what-is-a-discord-token-and-how-to-get-one/)
 - Run `npm install` to install dependencies
-  - I am on Node version 16.20.0 if it matters
+  - I am on NodeJS version 18.18.2if it matters
 - Run `node index.js` to run locally
-- Alternatively, I hosted this on [railway.app](https://railway.app/)
+- Code formatting: `npm run prettier`
+- For server hosting, I used [railway.app](https://railway.app/)
 
 ## Documentation
+
 Check [Discord.js v12 documentation](https://v12.discordjs.guide/creating-your-bot/commands-with-user-input.html), as well as [this tutorial I was using](https://www.alpharithms.com/how-to-code-a-discord-bot-in-javascript-444917/)
 
 ## Usage Instructions
+
 1. Add the bot to your Discord server using [this URL](https://discord.com/api/oauth2/authorize?client_id=1041152819874508812&permissions=2147486720&scope=bot%20applications.commands), or just DM the bot on Discord at `Timmy MK 3#9573`. Sending commands to the bot in a server and in DM's are both supported.
 2. The required inputs are to type `!time SCORE`, where SCORE is the score on the rating screen.
-    - You can have commas in your score input.
-3. Most video examples from [here](https://www.youtube.com/results?search_query=hitman+3+speedrun&sp=EgIYAQ%253D%253D) or [here](https://www.speedrun.com/hitman_3) will work for getting valid scores. Otherwise, any score value of about 50,000 to 210,000 should work. 
-    - **NOTE: Only Hitman level completion times of up to 5 minutes are compatible with the bot.** 
-    - Use the [Time Calc Website](https://solderq35.github.io/time-calc-under-5/over5) for times of 5 to 15 minutes.
+   - You can have commas in your score input.
+3. Most video examples from [here](https://www.youtube.com/results?search_query=hitman+3+speedrun&sp=EgIYAQ%253D%253D) or [here](https://www.speedrun.com/hitman_3) will work for getting valid scores. Otherwise, any score value of about 50,000 to 210,000 should work.
+   - **NOTE: Only Hitman level completion times of up to 5 minutes are compatible with the bot.**
+   - Use the [Time Calc Website](https://solderq35.github.io/time-calc-under-5/over5) for times of 5 to 15 minutes.
 4. For this example, let's use [this video](https://www.youtube.com/watch?v=zIRAmZdl-y4), which is an 8 second run of the "On Top of the World" Hitman level.
 5. We can see that the end of this video that the score at the end of the video is `83,521`. As shown here: ![Score](https://i.ibb.co/6v63rZm/goronscore.PNG)
 6. Now, let's type `!time 83521`.
 
 ![Milliseconds Score](https://i.ibb.co/sVpbXXd/timmy-example.png)
 
-7. As you can see, the bot returns a few viable solutions. One of them (8.981) is within a second of the 8 seconds value shown on the rating screen, so this is the milliseconds value we are looking for. 
+7. As you can see, the bot returns a few viable solutions. One of them (8.981) is within a second of the 8 seconds value shown on the rating screen, so this is the milliseconds value we are looking for.
+
+8. In case of rounding errors, you will get a warning, as shown below (example: `!time 113593`):
+
+![debug 1](https://i.ibb.co/mDgFDLH/debugtime1.png)
+
+9. Use the `!debugtime SCORE`, where SCORE is the score on the rating screen as shown below (example: `!debugtime 113593`):
+
+![debug 2](https://i.ibb.co/pfK6ZQT/debugtime2.PNG)
+
+If needed, click the individual links on the debug message to see the exact math equation use (cross reference with [Longer Math Explanation](https://github.com/solderq35/time-calc-under-5/blob/main/README.md#mathematics-code-explanation) link at bottom):
+
+![googleCalc](https://i.ibb.co/y4F9Qp5/googlecalc.png)
--- index.js
+++ index.js
@@ -8,8 +8,12 @@ const { Client, Intents } = require("discord.js");
 
 // Instantiate new client object with desired Intents
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES],
-  partials: ['CHANNEL',]
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGES,
+  ],
+  partials: ["CHANNEL"],
 });
 
 // Authenticate via environment variable having been loaded
@@ -23,90 +27,158 @@ client.on("ready", function (e) {
 
 // Wait for message events, check content for match,
 // respond cordially to user via reply.
-client.on("messageCreate", function (msg) {
+client.on("messageCreate", function (msgInput) {
   //console.log(msg)
 
-  const args = msg.content.slice(prefix.length).trim().split(" ");
-  const args2 = msg.content.trim().split(" ");
+  const args = msgInput.content.slice(prefix.length).trim().split(" ");
+  const args2 = msgInput.content.trim().split(" ");
   const command = args2.shift().toLowerCase();
-  if (command === "!time") {
+
+  // TESTING: change "time" to "testtime" or something to avoid triggering prod during local dev
+  if (command === "!time" || command === "!debugtime") {
     let score = args[1];
-	score = parseFloat(score.replace(/,/g, ''));
-	
+    score = parseFloat(score.replace(/,/g, ""));
+
     const result_array = [];
     // for all 20 possible M values
     for (let i = 1; i <= 20; i++) {
       // base M value multiplier (bonuses and penalties from rating screen)
-      var base_M = 5000 * i;
+      let base_M = 5000 * i;
 
-      // base result value (given in seconds:milliseconds. No rounding either.)
-      var base_result =
+      // base result value (given in seconds:milliseconds. No rounding yet)
+      let base_result =
         (parseFloat(210000) - parseFloat((score * 100000) / base_M)) *
+        parseFloat(3 / 400);
+
+      // error range (difference between upper bound ("base_result") and lower bound)
+      let error_range_result =
+        parseFloat((0.5 * 100000) / base_M) * parseFloat(3 / 400);
+
+      // lower bound result value (given in seconds:milliseconds. No rounding yet)
+      let lower_bound_result =
+        (parseFloat(210000) - parseFloat(((score + 0.5) * 100000) / base_M)) *
+        parseFloat(3 / 400);
+
+      // higher bound result value (given in seconds:milliseconds. No rounding yet)
+      let higher_bound_result =
+        (parseFloat(210000) - parseFloat(((score - 0.5) * 100000) / base_M)) *
         parseFloat(3 / 400);
 
       // only calculate if the base result value is within bounds of 0 - 5 minutes
       if (base_result > 0 && base_result < 300) {
-        var minutes = Math.floor(base_result / 60);
-        var seconds = base_result - minutes * 60;
+        let formatted_result = formatTime(base_result).formatted_result;
+        let formatted_lower_bound =
+          formatTime(lower_bound_result).formatted_result;
+        let lower_bound_seconds = formatTime(lower_bound_result).seconds;
+        let formatted_higher_bound =
+          formatTime(higher_bound_result).formatted_result;
+        let higher_bound_seconds = formatTime(higher_bound_result).seconds;
+        let error_range_seconds = formatTime(error_range_result).seconds;
 
-        // milliseconds we multiply by 1000 and then take remainder after dividing by 1000, to eliminate
-        // float division rounding errors
-        var milliseconds = Math.floor((seconds * 1000) % 1000);
-
-        // put the minutes / seconds / milliseconds together formatted
-        var formatted_result;
-        var formatted_seconds;
-        var formatted_milliseconds;
-
-        // add one leading zero to seconds digit if there are less than 10 seconds
-        if (seconds < 10) {
-          formatted_seconds = ":0" + Math.floor(seconds);
-        } else {
-          formatted_seconds = ":" + Math.floor(seconds);
+        // show 10^-5 seconds (thousandths of a millisecond) for error range delta (plus or minus from original time calc value)
+        // all ranges below are the same ms padding logic as in formatTime function, but with *100 for all ranges
+        let error_range_milliseconds = Math.floor(
+          (error_range_seconds * 100000) % 100000
+        );
+        let formatted_error_range =
+          formatTime(error_range_result).formatted_result;
+        if (error_range_milliseconds < 100000) {
+          if (error_range_milliseconds < 1000) {
+            formatted_error_range = "0.00" + error_range_milliseconds;
+          } else if (
+            error_range_milliseconds >= 1000 &&
+            error_range_milliseconds < 10000
+          ) {
+            formatted_error_range = "0.0" + error_range_milliseconds;
+          } else if (
+            error_range_milliseconds >= 10000 &&
+            error_range_milliseconds < 100000
+          ) {
+            formatted_error_range = "0." + error_range_milliseconds;
+          }
         }
-
-        // add two leading zeros to milliseconds digit if there are less than 10 milliseconds
-        if (milliseconds < 10) {
-          formatted_milliseconds = ".00" + (milliseconds % 1000);
+        let debug_result = `- Original Time Calc: [${formatted_result}](<https://www.google.com/search?q=%28210000+-+%28${parseFloat(
+          score
+        )}+*+100000+%2F+${base_M}%29%29+*+%283+%2F+400%29>)\n  - Time Calc Error Range: ([${formatted_lower_bound}](<https://www.google.com/search?q=%28210000+-+%28${parseFloat(
+          score + 0.5
+        )}+*+100000+%2F+${base_M}%29%29+*+%283+%2F+400%29>), [${formatted_higher_bound}](<https://www.google.com/search?q=%28210000+-+%28${parseFloat(
+          score - 0.5
+        )}+*+100000+%2F+${base_M}%29%29+*+%283+%2F+400%29>))\n  - Margin of Error (Seconds): ± [${formatted_error_range}](<https://www.google.com/search?q=%280.5+*+100000+%2F+${base_M}%29+*+%283+%2F+400%29>)\n  - M value: ${base_M}`;
+        if (command === "!time") {
+          result_array.push(formatted_result);
+          if (
+            Math.floor(lower_bound_seconds) !== Math.floor(higher_bound_seconds)
+          ) {
+            let debugTip =
+              "- Potential rounding error detected, for more info check `!debugtime SCORE`";
+            result_array.push(debugTip);
+          }
+        } else if (command === "!debugtime") {
+          result_array.push(debug_result);
         }
-
-        // add one leading zero to milliseconds digit if there are 10 to 99 milliseconds
-        else if (milliseconds >= 10 && milliseconds < 100) {
-          formatted_milliseconds = ".0" + (milliseconds % 1000);
-        } else {
-          formatted_milliseconds = "." + (milliseconds % 1000);
-        }
-
-        formatted_result = minutes + formatted_seconds + formatted_milliseconds;
-        result_array.push(formatted_result);
       }
     }
 
-    if (result_array.length > 0) {
-      var copymsg = "";
-      //result_array.unshift(copymsg);
-    }
+    let msgOutput = result_array.join("\n");
 
-    // if no numbers were returned in range (0 to 5 minutes), it's a bad input
-    else {
-      var copymsg = "Invalid Score :smiling_face_with_3_hearts:";
-      result_array.unshift(copymsg);
-    }
-
-    var msg2 = result_array.join("\n");
-
-    // regex for input validation (only numberic input accepted)
+    // regex for input validation (only numeric input accepted)
     let num_hyphen_check = /^[0-9]*$/;
     if (
-      (num_hyphen_check.test(score) == false ||
-        score > 210000 ||
-        score < 5000) &&
-      result_array.length > 0
+      num_hyphen_check.test(score) == false ||
+      score > 210000 ||
+      score < 5000 ||
+      // if no numbers were returned in range (0 to 5 minutes), it's a bad input
+      result_array.length === 0
     ) {
-      msg2 = "Invalid Score :smiling_face_with_3_hearts:";
+      msgOutput = "Invalid Score :smiling_face_with_3_hearts:";
     }
     //console.log(score);
-    var stringscore = msg2.toString();
-    msg.channel.send(stringscore);
+    if (command === "!debugtime") {
+      for (let i = 0; i < result_array.length; i++) {
+        msgInput.channel.send(result_array[i]);
+      }
+      let debugInfo =
+        "\nM value = Completed SA bonuses (20k each) minus nontarget kills (5k each)\n[Longer Math Explanation](<https://github.com/solderq35/time-calc-under-5/blob/main/README.md#mathematics-code-explanation>)";
+      msgInput.channel.send(debugInfo);
+    } else if (command === "!time") {
+      let stringscore = msgOutput.toString();
+      msgInput.channel.send(stringscore);
+    }
   }
 });
+
+function formatTime(base_result) {
+  let minutes = Math.floor(base_result / 60);
+  let seconds = base_result - minutes * 60;
+
+  // milliseconds we multiply by 1000 and then take remainder after dividing by 1000, to eliminate
+  // float division rounding errors
+  let milliseconds = Math.floor((seconds * 1000) % 1000);
+
+  // put the minutes / seconds / milliseconds together formatted
+  let formatted_result;
+  let formatted_seconds;
+  let formatted_milliseconds;
+
+  // add one leading zero to seconds digit if there are less than 10 seconds
+  if (seconds < 10) {
+    formatted_seconds = ":0" + Math.floor(seconds);
+  } else {
+    formatted_seconds = ":" + Math.floor(seconds);
+  }
+
+  // add two leading zeros to milliseconds digit if there are less than 10 milliseconds
+  if (milliseconds < 10) {
+    formatted_milliseconds = ".00" + (milliseconds % 1000);
+  }
+
+  // add one leading zero to milliseconds digit if there are 10 to 99 milliseconds
+  else if (milliseconds >= 10 && milliseconds < 100) {
+    formatted_milliseconds = ".0" + (milliseconds % 1000);
+  } else {
+    formatted_milliseconds = "." + (milliseconds % 1000);
+  }
+
+  formatted_result = minutes + formatted_seconds + formatted_milliseconds;
+  return { seconds, formatted_result };
+}
